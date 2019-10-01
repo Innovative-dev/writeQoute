b7dbe7462d530e7f1c0a3cb418642ed53c1cc296
Rambabu Meena <rambabumeena@LAP-C02VJDZFHV22.local>
2019-10-02 03:47:24 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navigation from './Navigation';
+import firebase from './firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+class App extends Component {
+  state = {
+    authenticated: false,
+  };
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((authenticated) => {
+      authenticated
+        ? this.setState(() => ({
+            authenticated: true,
+          }))
+        : this.setState(() => ({
+            authenticated: false,
+          }));
+    });
+  }
+  render() {
+    return <Navigation authenticated={this.state.authenticated} />;
+  }
+ }
+ export default App;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBTaPIfDB21tgp2z0jUNNpgB9U_85mkO4M",
+    authDomain: "ecommerce-demo-1317d.firebaseapp.com",
+    databaseURL: "https://ecommerce-demo-1317d.firebaseio.com",
+    projectId: "ecommerce-demo-1317d",
+    storageBucket: "",
+    messagingSenderId: "370489039013",
+    appId: "1:370489039013:web:5be09f4af1c578dbef954c",
+    measurementId: "G-1J7JF577V0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  export default firebase;
